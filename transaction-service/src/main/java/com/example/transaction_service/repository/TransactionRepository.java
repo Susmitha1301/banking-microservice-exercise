dbd2c25bc8df8cb8386c387540820f10343a9e4d
Susmitha <susmitha.dev48@gmail.com>
2026-05-01 15:00:43 -0400

    1C Repository Layer

---
+++ transaction-service/src/main/java/com/example/transaction_service/repository/TransactionRepository.java
@@ -0,0 +1,10 @@
+package com.example.transaction_service.repository;
+
+import com.example.transaction_service.entity.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TransactionRepository  extends JpaRepository<Transaction, Long> {
+    List<Transaction>  findByFromAccountNumberOrToAccountNumber(String fromAccountNumber, String toAccountNumber);
+}
